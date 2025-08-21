223551b233b2bec67fb8796809a43e57c71f1e58
jsg1224 <chilsung0731@gmail.com>
2025-08-21 19:49:25 +0900

    강사님 과제 추가

---
+++ python0821lab.py
@@ -0,0 +1,19 @@
+word = "Python"
+# 여기에 코드 작성
+print(word[3]) # 예상 결과: h
+print(word[-1])# 예상 결과: n
+
+numbers = [10, 20, 30, 40, 50]
+# 여기에 코드 작성
+print(numbers[2]) # 예상 결과: 30
+print(numbers[0]) # 예상 결과: 10
+
+text = "Python"
+# 여기에 코드 작성
+print(text[0:3]) # 예상 결과: Pyt
+print(text[2:]) # 예상 결과: thon
+
+data = [10, 20, 30, 40, 50]
+# 여기에 코드 작성
+print(numbers[1:4]) # 예상 결과: [20, 30, 40]
+print(numbers[4::-1])# 예상 결과: [50, 40, 30, 20, 10]
